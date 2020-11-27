2de3827589043bb8c0656cc4f897fcfb16112e24
Antoine <anthonyfg9@gmail.com>
2020-11-26 20:11:51 -0400

    part 10.12

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/react-hooks';
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
+  // console.log('Extra:', Constants.manifest.extra);
   return(
     <NativeRouter>
-      <PaperProvider>
-        <Main />
-      </PaperProvider>
+      <ApolloProvider client={apolloClient}>
+        <PaperProvider>
+          <Main />
+        </PaperProvider>
+      </ApolloProvider>
     </NativeRouter>
   );
 };
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,9 +1,12 @@
 import ApolloClient from 'apollo-boost';
+import { APOLLO_URI } from '@env';
 
 const createApolloClient = () => {
+    console.log('APOLLO_URI', APOLLO_URI);
     return new ApolloClient({
         // Replace the IP address part with your own IP address!
-        uri: 'http://192.168.0.22:5000/graphql',
+        // uri: 'http://192.168.0.22:5000/graphql',
+        uri: APOLLO_URI
     });
 };
 
