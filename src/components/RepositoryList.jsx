16041e877f2d56039d6ed169b5a799b7ff9b7bfd
Antoine <anthonyfg9@gmail.com>
2020-12-17 18:39:28 -0400

    refactoring repoList comp names

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,86 @@
 import React from 'react';
-import useRepositories from '../hooks/useRepositories';
-import RepositoryListContainer from './RepositoryListContainer';
+import { FlatList, View, StyleSheet, Image } from 'react-native';
+import { Card, Text } from 'react-native-paper';
 
-const RepositoryList = () => {
-  const { response } = useRepositories();
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  separator: {
+    height: 10,
+  },
+  avatarPic: {
+    width: 50,
+    height: 50,
+  },
+  chipRow: {
+    justifyContent: 'space-around',
+    marginTop: 10
+  },
+  chip: {
+    alignItems: 'center',
+    alignContent: 'center',
+    padding: 4
+  },
+  lastItemFix: {
+    height: '25px'
+  }
+});
 
-  // Get the nodes from the edges array
-  const repositories = response && response.edges ?
-    response.edges.map(edge => edge.node) : [];
+const ItemSeparator = () => <View style={styles.separator} />;
 
+const RepositoryList = ({repositories}) => {
   return (
-    <RepositoryListContainer repositories={ repositories } />
+    <FlatList
+      data={repositories}
+      ItemSeparatorComponent={ItemSeparator}
+      renderItem={({ item, index }) =>
+        <RepoCard key={index} repo={item} />
+      }
+    />
+  );
+};
+
+const RepoCard = ({ repo }) => {
+  return (
+    <Card testID={`repo-${repo.id}`}>
+      <Card.Title
+        title={repo.fullName}
+        subtitle={repo.description}
+        left={() => <Image style={styles.avatarPic} source={{ uri: repo.ownerAvatarUrl }} />}
+      // right={(props) => <IconButton {...props} icon="more-vert" onPress={() => { }} />}
+      />
+      <Card.Content>
+        <View style={styles.row}>
+          <View style={{ width: '10%', backgroundColor: '#FFFFFF' }} />
+          <View style={{ backgroundColor: '#4711BA', borderRadius: 5 }}>
+            <Text style={{ padding: 5 }}>
+              {repo.language}
+            </Text>
+          </View>
+        </View>
+        <View style={[styles.row, styles.chipRow]}>
+          <RepoChip bold={repo.stargazersCount} content={"Stars"} />
+          <RepoChip bold={repo.forksCount} content={"Forks"} />
+          <RepoChip bold={repo.reviewCount} content={"Reviews"} />
+          <RepoChip bold={repo.ratingAverage} content={"Rating"} />
+        </View>
+      </Card.Content>
+    </Card>
+  );
+};
+
+const RepoChip = ({ bold, content }) => {
+  return (
+    <View style={styles.chip}>
+      <Text style={{ fontWeight: 'bold' }}>
+        {bold}
+      </Text>
+      <Text>
+        {content}
+      </Text>
+    </View>
   );
 };
 
--- src/components/RepositoryListContainer.jsx
+++ src/components/RepositoryListContainer.jsx
@@ -1,86 +1,16 @@
 import React from 'react';
-import { FlatList, View, StyleSheet, Image } from 'react-native';
-import { Card, Text } from 'react-native-paper';
+import useRepositories from '../hooks/useRepositories';
+import RepositoryList from './RepositoryList';
 
-const styles = StyleSheet.create({
-  row: {
-    flex: 1,
-    flexDirection: 'row'
-  },
-  separator: {
-    height: 10,
-  },
-  avatarPic: {
-    width: 50,
-    height: 50,
-  },
-  chipRow: {
-    justifyContent: 'space-around',
-    marginTop: 10
-  },
-  chip: {
-    alignItems: 'center',
-    alignContent: 'center',
-    padding: 4
-  },
-  lastItemFix: {
-    height: '25px'
-  }
-});
+const RepositoryListContainer = () => {
+  const { response } = useRepositories();
 
-const ItemSeparator = () => <View style={styles.separator} />;
+  // Get the nodes from the edges array
+  const repositories = response && response.edges ?
+    response.edges.map(edge => edge.node) : [];
 
-const RepositoryListContainer = ({repositories}) => {
   return (
-    <FlatList
-      data={repositories}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item, index }) =>
-        <RepoCard key={index} repo={item} />
-      }
-    />
-  );
-};
-
-const RepoCard = ({ repo }) => {
-  return (
-    <Card testID={`repo-${repo.id}`}>
-      <Card.Title
-        title={repo.fullName}
-        subtitle={repo.description}
-        left={() => <Image style={styles.avatarPic} source={{ uri: repo.ownerAvatarUrl }} />}
-      // right={(props) => <IconButton {...props} icon="more-vert" onPress={() => { }} />}
-      />
-      <Card.Content>
-        <View style={styles.row}>
-          <View style={{ width: '10%', backgroundColor: '#FFFFFF' }} />
-          <View style={{ backgroundColor: '#4711BA', borderRadius: 5 }}>
-            <Text style={{ padding: 5 }}>
-              {repo.language}
-            </Text>
-          </View>
-        </View>
-        <View style={[styles.row, styles.chipRow]}>
-          <RepoChip bold={repo.stargazersCount} content={"Stars"} />
-          <RepoChip bold={repo.forksCount} content={"Forks"} />
-          <RepoChip bold={repo.reviewCount} content={"Reviews"} />
-          <RepoChip bold={repo.ratingAverage} content={"Rating"} />
-        </View>
-      </Card.Content>
-    </Card>
-  );
-};
-
-const RepoChip = ({ bold, content }) => {
-  return (
-    <View style={styles.chip}>
-      <Text style={{ fontWeight: 'bold' }}>
-        {bold}
-      </Text>
-      <Text>
-        {content}
-      </Text>
-    </View>
+    <RepositoryList repositories={ repositories } />
   );
 };
 
