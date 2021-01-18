4d2bbb753048bcbc582af1e4cce90982815fc078
Antoine <anthonyfg9@gmail.com>
2021-01-18 12:36:58 -0400

    adding options to review card

--- src/components/RepositoryReview.jsx
+++ src/components/RepositoryReview.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Card, Text, Avatar } from 'react-native-paper';
+import { View } from 'react-native';
+import { Card, Text, Avatar, IconButton } from 'react-native-paper';
+import { useHistory } from "react-router-native";
 
-const RepositoryReview = ({ review }) => {
-    return (
-      <Card>
-        <Card.Title
-          title={review.user.username}
-          subtitle={new Date(review.createdAt).toLocaleDateString('en-us')}
-          left={() => <Avatar.Text size={40} label={review.rating} />}
-        // right={(props) => <IconButton {...props} icon="more-vert" onPress={() => { }} />}
-        />
-        <Card.Content>
-          <Text>{review.text}</Text>
-        </Card.Content>
-      </Card>
-    );
+const RepositoryReview = ({ review, showReviewActions = false, setReview }) => {
+  const history = useHistory();
+  return (
+    <Card>
+      <Card.Title
+        title={review.user.username}
+        subtitle={new Date(review.createdAt).toLocaleDateString('en-us')}
+        left={() => <Avatar.Text size={40} label={review.rating} />}
+        right={(props) => showReviewActions ?
+          <View style={{ flexDirection: 'row' }}>
+            <IconButton icon='source-repository' onPress={() => {
+              console.log(`going to repo`, review);
+              history.push(`/repositories/${review.repositoryId}`);
+            }} />
+            <IconButton icon='delete' onPress={() => {setReview(review.id);}}/>
+          </View> : null
+        }
+      />
+      <Card.Content>
+        <Text>{review.text}</Text>
+      </Card.Content>
+    </Card>
+  );
 };
 
 export default RepositoryReview;
