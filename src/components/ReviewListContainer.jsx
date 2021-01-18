f882d7fdfb37e4f28752bd404b672b85737c4117
Antoine <anthonyfg9@gmail.com>
2021-01-18 12:38:28 -0400

    setting reviewListContainer

--- src/components/ReviewListContainer.jsx
+++ src/components/ReviewListContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, View, StyleSheet } from 'react-native';
 import useAuthorizedUser from '../hooks/useAuthorizedUser';
 import RepositoryReview from '../components/RepositoryReview';
@@ -11,15 +11,25 @@ const styles = StyleSheet.create({
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const ReviewListContainer = () => {
+const ReviewListContainer = ({ toggleDeleteReviewVisibility, setReviewIdToDelete }) => {
     const authorizedUser = useAuthorizedUser(true);
+    const setReview = id => {
+        console.log(`setting deleting review: ${id}`);
+        toggleDeleteReviewVisibility();
+        setReviewIdToDelete(id);
+    };
 
     return (
         authorizedUser.authorizedUser && authorizedUser.authorizedUser.reviews ?
             <FlatList
                 data={authorizedUser.authorizedUser.reviews.edges}
                 ItemSeparatorComponent={ItemSeparator}
-                renderItem={({ item, index }) => <RepositoryReview key={index} review={item.node} />}              
+                renderItem={({ item, index }) =>
+                    <RepositoryReview
+                        key={index}
+                        review={item.node}
+                        showReviewActions
+                        setReview={setReview}/>}  
             /> : null
     );
 };
