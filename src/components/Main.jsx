a3d003343abc0147f481644c1811e71da41d2a1f
Antoine <anthonyfg9@gmail.com>
2021-01-18 12:40:11 -0400

    part 10.27 adding review options

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Portal } from 'react-native-paper';
 import { Route, Switch, Redirect } from 'react-router-native';
@@ -9,12 +9,14 @@ import SignUpFormContainer from './SignUpFormContainer';
 import AddReviewFormContainer from './AddReviewFormContainer';
 import ReviewListContainer from './ReviewListContainer';
 import PickOrderMenu from './PickOrderMenu';
+import DeleteReview from './DeleteReview';
 import SearchBar from './SearchBar';
 import AppBar from './AppBar';
 import BottomBar from './BottomBar';
 import useSortMenuVisibility from '../hooks/useSortMenuVisibility';
 import useSortMenuState from '../hooks/useSortMenuState';
 import useSearchBarState from '../hooks/useSearchBarState';
+import useDeleteReviewState from '../hooks/useDeleteReviewState';
 
 const styles = StyleSheet.create({
   container: {
@@ -37,6 +39,8 @@ const Main = () => {
   const  { sortMenuVisibility, toggleSortMenuVisibility, closeSortMenu } = useSortMenuVisibility();
   const  { sortMenuCriteria, setSortMenuCriteria, sortMenuDirection, setSortMenuDirection } = useSortMenuState();
   const  { searchBarVisibility, toggleSearchBarVisibility, searchKeyword, setSearchKeyword } = useSearchBarState();
+  const  { deleteReviewVisibility, toggleDeleteReviewVisibility, reviewIdToDelete, setReviewIdToDelete } = useDeleteReviewState();
+
   return (
     <View style={styles.container}>
       <AppBar toggleSortMenuVisibility={toggleSortMenuVisibility} toggleSearchBarVisibility={toggleSearchBarVisibility} />
@@ -46,6 +50,7 @@ const Main = () => {
             sortMenuVisibility={sortMenuVisibility} closeSortMenu={closeSortMenu}
             sortMenuCriteria={sortMenuCriteria} setSortMenuCriteria={setSortMenuCriteria}
             sortMenuDirection={sortMenuDirection} setSortMenuDirection={setSortMenuDirection} />
+          <DeleteReview visibility={deleteReviewVisibility} toggleVisibility={toggleDeleteReviewVisibility} reviewIdToDelete={reviewIdToDelete} />
         </View>
       </Portal>
       <SearchBar searchBarVisibility={searchBarVisibility} searchKeyword={searchKeyword} setSearchKeyword={setSearchKeyword} />
@@ -63,7 +68,7 @@ const Main = () => {
           <AddReviewFormContainer />
         </Route>
         <Route path='/myReviews' exact>
-          <ReviewListContainer />
+          <ReviewListContainer toggleDeleteReviewVisibility={toggleDeleteReviewVisibility} setReviewIdToDelete={setReviewIdToDelete} reviewIdToDelete={reviewIdToDelete}/>
         </Route>
         <Route path='/signIn' exact>
           <SignInFormContainer />
